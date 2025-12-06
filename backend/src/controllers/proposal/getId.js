906775fb2de9db8b268b8e297052d633a7ef6ab5
anshif-8590 <anshifp949@gmail.com>
2025-12-06 21:49:24 +0530

    proposal get by id api completed

--- backend/src/controllers/proposal/getId.js
+++ backend/src/controllers/proposal/getId.js
@@ -1,10 +1,21 @@
+import Proposal from "../../models/Proposal.js";
 
 
 
 
 const getProposalById = async ( req , res ) => {
     try {
-        
+        const { id } = req.params
+        const proposal = await Proposal.findById( id )
+        .populate("rfpId")
+        .populate("vendorId")
+        console.log(proposal,"data")
+
+        if ( !proposal ) {
+            return res.status(404).json ({ message : "Proposal not found"})
+        }
+        res.json({ message : "Success" , proposal })
+
         
     } catch (error) {
          res.status(500).json({ message: error.message });
