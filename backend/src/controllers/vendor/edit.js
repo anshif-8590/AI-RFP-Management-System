b227821784970dda3ae47d59cbcec46bd4ce6d43
anshif-8590 <anshifp949@gmail.com>
2025-12-06 17:31:31 +0530

    vendor data edit api completd

--- backend/src/controllers/vendor/edit.js
+++ backend/src/controllers/vendor/edit.js
@@ -1,7 +1,19 @@
+import Vendor from "../../models/Vendor.js"
 
 
+const editVendor = async (req, res) => {
+    try {
+        const { id } = req.params
+        const data = req.body
+        const updatedVendorData = await Vendor.findByIdAndUpdate(id, data, { new: true })
+        if (!updatedVendorData) {
+            return res.status(404).json({ message: " Vendor not found " })
+        }
+        res.status(200).json({ message: "Success", newData: updatedVendorData })
+    } catch (error) {
+        res.status(500).json({ message: err.message });
+    }
 
-const editVendor = async ( req , res ) => {
 
 }
 
