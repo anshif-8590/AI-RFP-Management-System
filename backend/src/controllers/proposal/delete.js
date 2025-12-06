934b03aafb1a0021d9d8b66912eaf7d0db886bf9
anshif-8590 <anshifp949@gmail.com>
2025-12-06 21:56:13 +0530

    delete proposal api completed

--- backend/src/controllers/proposal/delete.js
+++ backend/src/controllers/proposal/delete.js
@@ -1,10 +1,13 @@
+import Proposal from "../../models/Proposal.js";
 
 
 
 
 const deleteProposal  = async ( req , res ) => {
     try {
-        
+        const { id } = req.params
+        await Proposal.findByIdAndDelete( id )
+        res.json({ message : "Proposal deleted "})
         
     } catch (error) {
          res.status(500).json({ message: error.message });
