360498681653a5d94c093be0ef37fcec063f6542
anshif-8590 <anshifp949@gmail.com>
2025-12-06 17:23:27 +0530

    vendor get by id api completed

--- backend/src/controllers/vendor/getId.js
+++ backend/src/controllers/vendor/getId.js
@@ -1,9 +1,20 @@
+import Vendor from "../../models/Vendor.js"
 
 
 
 
 const getVendorId = async ( req , res ) => {
+    try {
 
+        const { id } = req.params
+        const vendorData = await Vendor.find({ _id : id })
+        if ( vendorData.length === 0 ) {
+            return res.status(404).json({ message : " Vendor not found "})
+        }
+        res.status(200).json({ message : "Success" , data : vendorData})
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 
 export default getVendorId
