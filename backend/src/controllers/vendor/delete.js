9d79b822e32ab4a5a53ee659e3522ee034330ea1
anshif-8590 <anshifp949@gmail.com>
2025-12-06 17:35:21 +0530

    vendor delete api completed

--- backend/src/controllers/vendor/delete.js
+++ backend/src/controllers/vendor/delete.js
@@ -1,8 +1,18 @@
+import Vendor from "../../models/Vendor.js"
 
 
 
 const deleteVendor = async ( req , res ) => {
-
+    try {
+        const { id } = req.params
+        const vendorData = await Vendor.findByIdAndDelete( id )
+         if ( !vendorData ) {
+            return res.status(404).json({ message : "Vendor  not found "})
+        }
+        res.status(200).json({ message : "Vendor deleted successfully" , })
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 
 export default deleteVendor
