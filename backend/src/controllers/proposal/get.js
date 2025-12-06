828080caf9a605c73a2d38010f35014c64f8a663
anshif-8590 <anshifp949@gmail.com>
2025-12-06 21:53:15 +0530

    get all proposals api completed

--- backend/src/controllers/proposal/get.js
+++ backend/src/controllers/proposal/get.js
@@ -1,10 +1,15 @@
+import Proposal from "../../models/Proposal.js";
 
 
 
 
 const getAllProposal  = async ( req , res ) => {
     try {
+        const proposals = await Proposal.find()
+        .populate("rfpId")
+        .populate("vendorId")
         
+        res.json({ message : "Success" , proposals})
         
     } catch (error) {
          res.status(500).json({ message: error.message });
