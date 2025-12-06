8d5104377acbb682efc4d719fb70205035f8ea8a
anshif-8590 <anshifp949@gmail.com>
2025-12-07 04:31:47 +0530

    setted the tailwind css and started pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,11 @@ function App() {
   }, []);
   return (
     <>
-
+  <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-green-400">
+        Tailwind is working 🚀
+      </h1>
+    </div>
     </>
   );
 }
